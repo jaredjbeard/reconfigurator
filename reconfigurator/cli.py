f4667a96347fd978664976f35703f5e2e67c2180
Jared Beard <jbeard6@mix.wvu.edu>
2023-01-18 11:44:10 -0500

    rm cli, test

--- reconfigurator/cli.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-"""
-This script is intended to run single or multithreaded decision making experiments
-"""
-__license__ = "BSD-3"
-__docformat__ = 'reStructuredText'
-__author__ = "Jared Beard"
