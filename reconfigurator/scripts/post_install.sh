c9c7b1bc1ff441354b3bd589535e259afa67651d
ThebeardedOn3 <jbeard6@mix.wvu.edu>
2023-01-19 18:19:50 -0500

    fixed path update

--- reconfigurator/scripts/post_install.sh
+++ reconfigurator/scripts/post_install.sh
@@ -11,7 +11,8 @@ if [[ $add_cli == "Y" || $add_cli == "y" ]]; then
     chmod +x reconfigurator/reconfigurator.py
     ln -s ../reconfigurator/reconfigurator.py bin/reconfigurator
     
-    export_path='export PATH="$PATH:$parent/../bin/"'
+    export_path='export PATH="$PATH:parent/../bin/"'
+    export_path=${export_path/parent/$parent}
     echo $export_path >> ~/.bashrc
     source ~/.bashrc
 
