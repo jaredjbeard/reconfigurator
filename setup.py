03968295d85940b43ee670b6cccc1e856431b5ac
ThebeardedOn3 <jbeard6@mix.wvu.edu>
2023-01-19 18:09:01 -0500

    added cli

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+import os
+
+os.chmod('reconfigurator/scripts/post_install.sh', 0o755) #rwxr-xr-x
+
 setup(
     name="reconfigurator",
     version="0.0.1",
-    install_requires=["nestifydict",
-                      ],
+    install_requires=[],
+    scripts=['reconfigurator/scripts/post_install.sh'],
+    data_files=[('scripts', ['reconfigurator/scripts/post_install.sh'])],
 )
