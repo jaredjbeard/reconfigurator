51c2d7349bb39be8f2effbf13f2dabc808e2da6c
ThebeardedOn3 <jbeard6@mix.wvu.edu>
2023-01-19 20:28:40 -0500

    integrated CLI, experimental features

--- setup.py
+++ setup.py
@@ -1,13 +1,7 @@
 from setuptools import setup
 
-import os
-
-os.chmod('reconfigurator/scripts/add_cli.sh', 0o755) #rwxr-xr-x
-
 setup(
     name="reconfigurator",
     version="0.0.1",
     install_requires=[],
-    scripts=['reconfigurator/scripts/add_cli.sh'],
-    data_files=[('scripts', ['reconfigurator/scripts/add_cli.sh'])],
 )
