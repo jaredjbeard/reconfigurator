e5e19f5bf040d971bd122e02d606290335dca300
ThebeardedOn3 <jbeard6@mix.wvu.edu>
2023-01-20 18:16:50 -0500

    updated conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,6 +5,9 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, os.path.abspath("../../reconfigurator/"))
 
 project = 'reconfigurator'
 copyright = '2023, Jared J. Beard'
@@ -14,7 +17,7 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +27,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:
+  html_theme = 'sphinx_rtd_theme'
+  html_static_path = ['_static']
