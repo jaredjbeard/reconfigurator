0462d41764390d29544f2c2854cb0d83634dbe47
ThebeardedOn3 <jbeard6@mix.wvu.edu>
2023-02-03 16:55:19 -0500

    fixed build

--- reconfigurator/scripts/add_cli.bash
+++ reconfigurator/scripts/add_cli.bash
@@ -6,15 +6,15 @@ if ! test -d "bin/"; then
     mkdir bin
 fi
 
-chmod +x reconfigurator/reconfigurator.py
+chmod +x reconfigurator.py
 
 save_loc=bin/reconfigurator
 if test -f "$save_loc"; then
     rm "$save_loc"
 fi
-ln -s ../reconfigurator/reconfigurator.py $save_loc
+ln -s ../reconfigurator.py $save_loc
     
-export_path='export PATH="$PATH:parent/../bin/"'
+export_path='export PATH="$PATH:parent/bin/"'
 export_path=${export_path/parent/$parent}
 
 if grep -Fxq "$export_path" ~/.bashrc; then
